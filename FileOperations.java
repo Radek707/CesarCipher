029e0f8770ba924e4ee5595a0ae4f877ec303689
Radek707 <rnowosielski77@gmail.com>
2022-05-19 23:11:08 +0200

    readFile method improved to read all lines

--- FileOperations.java
+++ FileOperations.java
@@ -10,16 +10,27 @@ public class FileOperations {
 
     static String readFile(String path) {
         File file = new File(path);
+        BufferedReader reader = null;
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
             String line;
 
             while ((line = reader.readLine()) != null) {
-                return line;
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
             }
+
+            return stringBuilder.toString();
         } catch (IOException e) {
             System.out.println("Something went wrong with reading the file");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
